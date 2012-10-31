403b4b28e12f229b31769ef491feed663ab1fd76
Esteve Fernandez <esteve.fernandez@gmail.com>
2012-10-31 20:54:46 +0100

    avoid multiple declaration of Comm

--- include/r2d2/comm.hpp
+++ include/r2d2/comm.hpp
@@ -1,3 +1,6 @@
+#ifndef R2D2_COMM_HPP
+#define R2D2_COMM_HPP
+
 #include <cstdint>
 
 class Comm {
@@ -6,3 +9,4 @@ public:
     virtual void devRead(uint8_t *, int) = 0;
     virtual bool open() = 0;
 };
+#endif
