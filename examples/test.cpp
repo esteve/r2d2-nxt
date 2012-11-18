29cfd0ec7405a34eebf5c788aef95901e6b558a4
Esteve Fernandez <esteve.fernandez@gmail.com>
2012-11-18 16:47:19 +0100

    Fix style.

--- examples/test.cpp
+++ examples/test.cpp
@@ -9,17 +9,15 @@ int main()
 
     USBNXTManager usbm;
     NXT* nxt = usbm.list()->at(0);
-    if(nxt->open()) //initialize the NXT and continue if it succeds
-        {
-            Sensor* sensor = nxt->makeTouch(NXT::IN_1); //tell the NXT that the touch sensor is in port 1
-            Motor* motor = nxt->makeMotor(NXT::OUT_A);
-                while(1) //main loop
-                {
-                        if(sensor->getValue() == true) //if the touch sensor is pressed down...
-                                motor->setForward(50); //turn the motor in port 1 on 50% power
-                        else
-                                motor->stop(false); //if the touch sensor is not pressed down turn the motor off
-                }
+    if (nxt->open()) { //initialize the NXT and continue if it succeds
+        Sensor* sensor = nxt->makeTouch(NXT::IN_1); //tell the NXT that the touch sensor is in port 1
+        Motor* motor = nxt->makeMotor(NXT::OUT_A);
+        while (1) { //main loop
+            if (sensor->getValue() == true) //if the touch sensor is pressed down...
+                motor->setForward(50); //turn the motor in port 1 on 50% power
+            else
+                motor->stop(false); //if the touch sensor is not pressed down turn the motor off
+        }
     }
-        return 0;
+    return 0;
 }
