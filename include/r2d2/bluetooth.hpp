52be1d71a0f6ede6d77f4feff37075ee3843d2d0
Esteve Fernandez <esteve@apache.org>
2013-01-08 22:38:18 +0100

    Added missing bluetooth header

---
+++ include/r2d2/bluetooth.hpp
@@ -0,0 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef R2D2_BLUETOOTH_HPP
+#define R2D2_BLUETOOTH_HPP
+#include <vector>
+#include <r2d2.hpp>
+
+class BTBrickManager : public BrickManager {
+    static const int NXT_VENDOR_ID = 0x0694;
+    static const int NXT_PRODUCT_ID = 0x0002;
+
+public:
+    std::vector<Brick *>* list();
+};
+#endif
